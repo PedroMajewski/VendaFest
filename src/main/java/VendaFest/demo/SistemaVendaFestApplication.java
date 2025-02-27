bf9043ce6555185c7293adb2c40e5094e1ce876c
PedroMajewski <pedrohmssilva@gmail.com>
2025-02-27 20:29:36 -0300

    First commit

---
+++ src/main/java/VendaFest/demo/SistemaVendaFestApplication.java
@@ -0,0 +1,13 @@
+package VendaFest.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVendaFestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaVendaFestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/VendaFest/demo/SistemaVendaFestApplicationTests.java
@@ -0,0 +1,13 @@
+package VendaFest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaVendaFestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
